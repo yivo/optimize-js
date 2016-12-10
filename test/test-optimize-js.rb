e5a037cb10a2fdd8c0e1409b3d11b94f487616d2
Yaroslav Konoplov <eahome00@gmail.com>
2016-12-10 23:26:26 +0200

    Update test

--- test/test-optimize-js.rb
+++ test/test-optimize-js.rb
@@ -5,7 +5,9 @@ require 'optimize-js'
 require 'test/unit'
 
 class OptimizeJSTest < Test::Unit::TestCase
-  def test_optimize_js
-    assert_equal '!(function() {})()', OptimizeJS.perform('!function() {}()')
+  def test_api
+    assert_equal OptimizeJS.perform('!function() {}()'), '!(function() {})()'
+    assert_match /optimize-js\.web\.min\.js/, OptimizeJS.js_path
+    assert_match /optimize/, OptimizeJS.js_function
   end
 end
