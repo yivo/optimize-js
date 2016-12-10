9776075734b4347e9eb4becb452cf5d64d0beb7c
Yaroslav Konoplov <eahome00@gmail.com>
2016-12-10 23:25:34 +0200

    Fix typo in OptimizeJS#js_path

--- lib/optimize-js.rb
+++ lib/optimize-js.rb
@@ -6,13 +6,13 @@ require 'execjs'
 module OptimizeJS
   class << self
     def perform(js, options = {})
-      @optimize_js ||= ExecJS.runtime.compile(js_path)
+      @optimize_js ||= ExecJS.runtime.compile(File.read(js_path))
       raise OptimizeJS::CompileError unless @optimize_js
       @optimize_js.call(js_function, js, options)
     end
 
     def js_path
-      File.read(File.expand_path('../optimize-js.web.min.js', File.dirname(__FILE__)))
+      File.expand_path('../optimize-js.web.min.js', File.dirname(__FILE__))
     end
 
     def js_function
